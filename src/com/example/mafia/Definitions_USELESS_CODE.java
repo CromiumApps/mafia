d84783a78bc03a142e8f2b9e5ec1d67f9dd95aa2
Habeeb Ahmed <Habeeb Ahmed@172.21.85.28>
2012-11-15 10:45:21 -0500

    for some reason some files are not getting commited

---
+++ src/com/example/mafia/Definitions_USELESS_CODE.java
@@ -0,0 +1,20 @@
+package com.example.mafia;
+
+public class Definitions_USELESS_CODE {
+
+}
+
+
+//These are all definitions of things in the code that need to be explained
+//add any non- intuitive things you need to explain here
+/*
+
+Dictionary:
+-player types
+	- 0 = civillian
+	- 1 = mafia
+	- 2 = milkman
+	- 3 = detective
+	- 4 = doctor 
+
+*/
