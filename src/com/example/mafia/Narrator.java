7edb9d06561cd9f5259fc6b5cb984eeeec7699ed
Kevin <kevin.j.carruthers@gmail.com>
2012-11-23 23:04:23 -0500

    Optimized randomization

--- src/com/example/mafia/Narrator.java
+++ src/com/example/mafia/Narrator.java
@@ -9,8 +9,7 @@ public class Narrator {
 		//data.milkGood
 		//data.drankMilk
 
-		// What if doctor saves himself?
-
+		Random random = new Random();
 		int n, m;
 		if(data.drankMilk) {
 			if(data.milkGood) {
@@ -20,7 +19,6 @@ public class Narrator {
 
 					savedByTheMilk[0] = " opened his front foor this morning and found a tall glass of fresh milk sitting on his doorstep. He took a deep sip of the refreshing liquid and noticed a car with tinted windows race past him. As it passed him, shots rang out: weapon fire from at least three different submachine guns. As if protected by magic, each and every bullet missed him, and crashed into the ground beneath his feet. He quickly finished his milk and went inside.";
 
-					Random random = new Random();
 					random.nextInt(n);
 
 					sequentialPrint(data.killed.getName() + savedByTheMilk[random]);
@@ -36,7 +34,6 @@ public class Narrator {
 
 					badMilk[0] = " was delivered a fresh cup of milk by the friendly neighbourhood milkman this morning. His mouth watering, he quickly drained the glass. After only a moment, though, his throat began to constrict, and he fell to the ground, unable to breath.";
 
-					Random random = new Random();
 					random.nextInt(n);
 
 					sequentialPrint(data.givenMilk + badMilk[random]);
@@ -54,14 +51,8 @@ public class Narrator {
 
 					badMilkOwnDoctor[0] = " drank some terrible milk this morning. Fortunately, he already had the antidote in his system, thanks to a prescient doctor. He'll be OK.";
 
-					if(dataPasser.getDoctor() == data.saved) {
-						Random random = new Random();
-						random.nextInt(m);
-					}
-					else {
-						Random random = new Random();
-						random.nextInt(n);
-					}
+					if(dataPasser.getDoctor() == data.saved) random.nextInt(m);
+					else random.nextInt(n);
 
 					sequentialPrint(data.givenMilk + badMilkGoodDoctor[random]);
 
@@ -76,8 +67,13 @@ public class Narrator {
 
 			luckyDoctor[0] = " was walking to his mailbox this morning when he noticed that the sky was rapidly darkening. He looked up in confusion, only to find a piano falling towards him. He hardly had time to scream before the piano crushed him to death, but scream he did. Fortunately for him, a local passerby heard his scream, sprinted over to him, and pushed him out of the way of the piano.";
 
-			Random random = new Random();
-			random.nextInt(n);
+			m = 1;
+			saveYourself = new String[m];
+
+			saveYourself[0] = " was hit by a car this morning. Fortunately, there was a doctor nearby who managed to re-attach his head before he could lose too much blood. He'll be fully recovered shortly."
+
+			if(dataPasser.getDoctor() == data.saved) random.nextInt(m);
+			else { random.nextInt(n);
 
 			sequentialPrint(data.killed + luckyDoctor[random]);
 
@@ -90,7 +86,6 @@ public class Narrator {
 
 			unluckyVictim[0] = " took the bus to work this morning. Just as he had gotten off the bus and was walking toward his office, he fell into a large hole in the sidewalk. Sadly, it was filled with spikes, and he died instantly.";
 
-			Random random = new Random();
 			random.nextInt(n);
 
 			sequentialPrint(data.killed + unluckyVictim[random]);
@@ -116,7 +111,6 @@ public class Narrator {
 
 			weKillTheKillers[0] = "The sun rose over a new day. The streets were clean, the sky bright, and people had stopped dying for no apparent reason. Throughout the city, people rejoiced, for the mafia were dead. Congratulations, civilian!";
 
-			Random random = new Random();
 			random.nextInt(n);
 
 			sequentialPrint(weKillTheKillers[random]);
@@ -128,7 +122,6 @@ public class Narrator {
 
 			idiotPlebeians[0] = "The city is yours, cleansed of all the civilians you could find. Of course, now there are no doctors, firemen, public service workers... other people... But, anyway, you accomplished what you set out to do, so congratulations!";
 
-			Random random = new Random();
 			random.nextInt(n);
 
 			sequentialPrint(idiotPlebeians[random]);
